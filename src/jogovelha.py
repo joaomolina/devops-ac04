00f5459b0b4a921cd0ead27320c9bdf06e8361df
rodrigobernucio <36374195+rodrigobernucio@users.noreply.github.com>
2018-03-16 20:51:20 -0300

    Update jogovelha.py

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -1,34 +1,14 @@
 def inicializar():
-tab = [ ]
-for i in range(3):
-linha = [ ]
-for j in range(3):
-linha.append(".")
-tab.append(linha)
-return tab
-def main( ):
-jogo = inicializar( )
-print (jogo)
-if __name__ == "__main__":
-main()
-Passo 6
-• Edite o conteúdo do arquivo src/testes.py para que fique
-com o conteúdo abaixo:
-Import jogovelha
-import sys
-erroInicializar = False
-jogo = jogovelha.inicializar()
-if len(jogo) != 3:
-erroInicializar = True
-else:
-for linha in jogo:
-if len(linha) != 3:
-erroInicializar = True
-else:
-for elemento in linha:
-if elemento != '.':
-erroInicializar = True
-if erroInicializar:
-sys.exit(1)
-else:
-sys.exit(0)
+    tab = [ ]
+    for i in range(3):
+        linha = [ ]
+    for j in range(3):
+        linha.append(".")
+        tab.append(linha)
+    return tab
+    
+derf main( ):
+    jogo = inicializar( )
+    print (jogo)
+    if __name__ == "__main__":
+        main()
